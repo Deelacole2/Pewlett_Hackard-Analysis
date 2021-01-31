b4dc948ff722e3a0eeee5d66041fb79f66a6d084
Dana Rogers <rogers.d.207@gmail.com>
2021-01-31 12:16:39 -0600

    Adding updated code snippets and visuals for the READ.ME

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -48,3 +48,60 @@ ON (e.emp_no = t.emp_no)
 WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 AND (de.to_date = '9999-01-01')
 ORDER BY e.emp_no;
+
+
+------------------------- Challenge summary code
+-- Count of Retiring Staff
+SELECT COUNT(retirees.emp_no)
+FROM (SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC) as retirees;
+
+-- Count of Mentees -- 1549
+SELECT COUNT(mentees.emp_no)
+FROM (SELECT DISTINCT ON(e.emp_no) e.emp_no, 
+    e.first_name, 
+    e.last_name, 
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+--INTO mentorship_eligibility
+FROM employees as e 
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no) as mentees;
+
+--By Dept
+
+SELECT by_dept.dept_no, COUNT(by_dept.dept_no), by_dept.dept_name
+FROM (SELECT DISTINCT ON(e.emp_no) e.emp_no, 
+		e.first_name, 
+		e.last_name, 
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		de.dept_no,
+	    d.dept_name,
+		t.title
+	-- INTO 
+	FROM employees as e 
+	INNER JOIN dept_emp as de
+	ON (e.emp_no = de.emp_no)
+	INNER JOIN titles as t
+	ON (e.emp_no = t.emp_no)
+	INNER JOIN departments as d
+	ON (de.dept_no = d.dept_no)
+	WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (de.to_date = '9999-01-01')
+	ORDER BY e.emp_no) as by_dept
+GROUP BY by_dept.dept_no, by_dept.dept_name
+ORDER BY COUNT DESC;
+
